dedf969309d91604d30438144a1895ac17eebe90
SURYA K <131654704+surya03-it@users.noreply.github.com>
2023-08-09 11:13:02 +0530

    Add files via upload

---
+++ Python code/creditcard_detection.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
+
+# Step 1: Load the historical transaction data
+data = pd.read_csv("creditcard .csv")
+# Step 2: Data Preprocessing
+# Assuming you have already handled any missing values and encoded categorical features
+X = data.drop("Class", axis=1)
+y = data["Class"]
+
+# Step 3: Data Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Step 4: Feature Scaling
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Step 5: Model Training
+model = LogisticRegression()
+model.fit(X_train_scaled, y_train)
+
+# Step 6: Model Evaluation
+y_pred = model.predict(X_test_scaled)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("ROC-AUC Score:", roc_auc)
+print("Confusion Matrix:")
+print(confusion)
+
